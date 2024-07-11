de17f6d1694f3f5e047b5d79bfb721bb85839a1e
tapskabs <141030379+tapskabs@users.noreply.github.com>
2024-07-11 20:22:51 +0200

    Boss

---
+++ Assets/BossChase.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossChase : MonoBehaviour
+{
+    public GameObject bossPrefab; 
+    public Transform player; 
+    public float bossSpeed = 3f; 
+
+    private static GameObject bossInstance;
+
+  
+    void Start()
+    {
+        if (bossInstance == null)
+        {
+            SpawnBoss();
+        }
+    }
+
+    void Update()
+    {
+        if (bossInstance != null)
+        {
+            
+            Vector2 direction = (player.position - bossInstance.transform.position).normalized;
+            bossInstance.transform.position = Vector2.MoveTowards(bossInstance.transform.position, player.position, bossSpeed * Time.deltaTime);
+        }
+    }
+
+    void SpawnBoss()
+    {
+        if (bossInstance == null)
+        {
+ 
+            Vector2 spawnPosition = new Vector2(0, 0);
+            bossInstance = Instantiate(bossPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
