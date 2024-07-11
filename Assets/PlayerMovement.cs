1bc182066e10369c385a264f89c9a34c46c10bb3
Lilycorpzz <143013925+Lilycorpzz@users.noreply.github.com>
2024-07-11 17:00:33 +0200

    Size and edible count

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,11 +9,14 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    public float sizeIncreaseAmount = 0.1f;
+    private int edibleCount;
 
-   
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        edibleCount = 0;
     }
 
     void Update()
@@ -30,8 +34,24 @@ public class PlayerMovement : MonoBehaviour
     {   
         if (other.gameObject.CompareTag("Edible"))
         {
-          
+            edibleCount++;
             Destroy(other.gameObject);
+            Debug.Log("Food eaten" + edibleCount);
+            GetBig();
+            //Debug.Log("Food eaten" + edibleCount);
+            
         }
+       
+    }
+    public void GetBig()
+    {
+        Vector3 newSize = transform.localScale;
+        newSize += new Vector3(sizeIncreaseAmount, sizeIncreaseAmount, 0); // Adjust this line for 3D scaling if needed
+        transform.localScale = newSize;
+    }
+
+    public int GetEdibleCount()
+    {
+        return edibleCount; // Return the count of destroyed edible objects
     }
 }
